73a34b0a2a22f996234a53cbdca2aa14da20826f
alburquenqueletelier <alburquenque.letelier@gmail.com>
2021-07-08 21:26:11 -0400

    Se quito view (search) pero aun no funciona el query

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:select>", views.show, name="show"),
+    path("busqueda", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,5 @@
+from re import search
+from django.http.response import HttpResponse
 from django.shortcuts import render
 import markdown2
 from . import util
@@ -5,15 +7,33 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-class search(forms.Form):
-    search_ = forms.CharField(label="Search Encyclopedia")
+class busqueda(forms.Form):
+    q = forms.CharField(label="Search wiki")
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    context = {
+        "entries": util.list_entries(),
+        "search_q": busqueda(),
+        "search" : request.session["search"]
+    }
+    if "search" not in request.session:
+        request.session["search"] = []
+    
+    if request.method == "POST":
+        search_q = busqueda(request.POST)
+        if search_q.is_valid():
+            q = search_q.cleaned_data["q"]
+            request.session["search"] = [q]
+            HttpResponseRedirect(reverse('encyclopedia:show'), args=search)
+        else:
+            return render(request, "encyclopedia/index.html",{
+                "search_q" : search_q
+            })
+    
+    return render(request, "encyclopedia/index.html", context)
 
 
 def show(request, select):
-    see = {'select' : select, 'see' : markdown2.markdown(util.get_entry(select))}
+    see = {'select' : select, 'see' : markdown2.markdown(util.get_entry(select)), "search_q" : busqueda() }
     return render(request, "encyclopedia/show.html", see)
+
